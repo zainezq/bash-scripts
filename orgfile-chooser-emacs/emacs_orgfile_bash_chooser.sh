2819096aafdfe5a3f12faea75c6e1693a136b10e
zaine <zaine@192.168.0.148>
2024-09-25 12:07:57 +0100

    added two new bash scripts

---
+++ orgfile-chooser-emacs/emacs_orgfile_bash_chooser.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# File paths
+files=(
+    "/home/..."
+    "/home/..."
+    "/home/..."
+)
+
+# Display options to the user
+echo "Please choose a number corresponding to the file you want to open:"
+for i in "${!files[@]}"; do
+    filename=$(basename "${files[$i]}")
+    echo "$((i+1))) $filename"
+done
+echo "4) Open all files"
+echo "5) Add a new file to the list"
+echo "6) Exit"
+
+# Read the user's input
+read -p "Enter your choice (1/2/3/4/5/6): " choice
+
+# Define actions based on user's choice
+case $choice in
+    1|2|3)
+        selected_file="${files[$((choice-1))]}"
+        if [ -f "$selected_file" ]; then
+            emacs "$selected_file" &
+        else
+            echo "Error: File '$selected_file' not found."
+        fi
+        ;;
+    4)
+        for file in "${files[@]}"; do
+            if [ -f "$file" ]; then
+                emacs "$file" &
+            else
+                echo "Error: File '$file' not found."
+            fi
+        done
+        ;;
+    5)
+        read -p "Enter the full path of the new file to add: " new_file
+        if [ -f "$new_file" ]; then
+            files+=("$new_file")
+            echo "File added: $new_file"
+        else
+            echo "Error: File '$new_file' not found."
+        fi
+        ;;
+    6)
+        echo "Exiting."
+        exit 0
+        ;;
+    *)
+        echo "Invalid option. Please run the script again and choose a valid number."
+        ;;
+esac
---
+++ orgfile-chooser-emacs/emacs_orgfile_bash_chooser_2.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Path to the file list
+FILE_LIST="$HOME/path/to/file_list.txt"
+
+# Check if file_list.txt exists, if not, create it with default files
+if [ ! -f "$FILE_LIST" ]; then
+    echo "$HOME/path/to/default1.org" > "$FILE_LIST"
+    echo "$HOME/path/to/default2.org" >> "$FILE_LIST"
+    echo "$HOME/path/to/default3.org" >> "$FILE_LIST"
+fi
+
+# Read file paths from the file list into an array
+mapfile -t files < "$FILE_LIST"
+
+# Display options to the user
+echo "Please choose a number corresponding to the file you want to open:"
+for i in "${!files[@]}"; do
+    filename=$(basename "${files[$i]}")
+    echo "$((i+1))) $filename"
+done
+echo "$(( ${#files[@]} + 1 ))) Open all files"
+echo "$(( ${#files[@]} + 2 ))) Add a new file to the list"
+echo "$(( ${#files[@]} + 3 ))) Exit"
+
+# Read the user's input
+read -p "Enter your choice: " choice
+
+# Define actions based on user's choice
+case $choice in
+    [1-$((${#files[@]}))])
+        selected_file="${files[$((choice-1))]}"
+        if [ -f "$selected_file" ]; then
+            emacs "$selected_file" &
+        else
+            echo "Error: File '$selected_file' not found."
+        fi
+        ;;
+    $((${#files[@]} + 1)))
+        for file in "${files[@]}"; do
+            if [ -f "$file" ]; then
+                emacs "$file" &
+            else
+                echo "Error: File '$file' not found."
+            fi
+        done
+        ;;
+    $((${#files[@]} + 2)))
+        read -p "Enter the full path of the new file to add: " new_file
+        if [ -f "$new_file" ]; then
+            # Append the new file to the array and the file_list.txt
+            echo "$new_file" >> "$FILE_LIST"
+            echo "File added: $new_file"
+        else
+            echo "Error: File '$new_file' not found."
+        fi
+        ;;
+    $((${#files[@]} + 3)))
+        echo "Exiting."
+        exit 0
+        ;;
+    *)
+        echo "Invalid option. Please run the script again and choose a valid number."
+        ;;
+esac
